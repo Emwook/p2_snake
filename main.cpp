d4a08d4b63367ea139f6436cabf5a740d755e26f
Emwook <lukasiuk.emilia@gmail.com>
2025-01-08 11:45:13 +0100

    snake head display and direction change with arrow keys

--- main.cpp
+++ main.cpp
@@ -12,8 +12,8 @@ extern "C" {
 #define SCREEN_HEIGHT	480
 
 
-// narysowanie napisu txt na powierzchni screen, zaczynaj¹c od punktu (x, y)
-// charset to bitmapa 128x128 zawieraj¹ca znaki
+// narysowanie napisu txt na powierzchni screen, zaczynajï¿½c od punktu (x, y)
+// charset to bitmapa 128x128 zawierajï¿½ca znaki
 // draw a text txt on surface screen, starting from the point (x, y)
 // charset is a 128x128 bitmap containing character images
 void DrawString(SDL_Surface *screen, int x, int y, const char *text,
@@ -40,7 +40,7 @@ void DrawString(SDL_Surface *screen, int x, int y, const char *text,
 
 
 // narysowanie na ekranie screen powierzchni sprite w punkcie (x, y)
-// (x, y) to punkt œrodka obrazka sprite na ekranie
+// (x, y) to punkt ï¿½rodka obrazka sprite na ekranie
 // draw a surface sprite on a surface screen in point (x, y)
 // (x, y) is the center of sprite on screen
 void DrawSurface(SDL_Surface *screen, SDL_Surface *sprite, int x, int y) {
@@ -62,8 +62,8 @@ void DrawPixel(SDL_Surface *surface, int x, int y, Uint32 color) {
 	};
 
 
-// rysowanie linii o d³ugoœci l w pionie (gdy dx = 0, dy = 1) 
-// b¹dŸ poziomie (gdy dx = 1, dy = 0)
+// rysowanie linii o dï¿½ugoï¿½ci l w pionie (gdy dx = 0, dy = 1) 
+// bï¿½dï¿½ poziomie (gdy dx = 1, dy = 0)
 // draw a vertical (when dx = 0, dy = 1) or horizontal (when dx = 1, dy = 0) line
 void DrawLine(SDL_Surface *screen, int x, int y, int l, int dx, int dy, Uint32 color) {
 	for(int i = 0; i < l; i++) {
@@ -74,7 +74,7 @@ void DrawLine(SDL_Surface *screen, int x, int y, int l, int dx, int dy, Uint32 c
 	};
 
 
-// rysowanie prostok¹ta o d³ugoœci boków l i k
+// rysowanie prostokï¿½ta o dï¿½ugoï¿½ci bokï¿½w l i k
 // draw a rectangle of size l by k
 void DrawRectangle(SDL_Surface *screen, int x, int y, int l, int k,
                    Uint32 outlineColor, Uint32 fillColor) {
@@ -87,6 +87,15 @@ void DrawRectangle(SDL_Surface *screen, int x, int y, int l, int k,
 		DrawLine(screen, x + 1, i, l - 2, 1, 0, fillColor);
 	};
 
+// own function
+void ResetGame(double &posX, double &posY, double &worldTime, double &distance, double &etiSpeed, int &direction) {
+    posX = SCREEN_WIDTH / 2;
+	posY = SCREEN_HEIGHT / 2;
+	worldTime = 0;
+	distance = 0;
+	etiSpeed = 1;
+	direction = 0;
+}
 
 // main
 #ifdef __cplusplus
@@ -94,7 +103,7 @@ extern "C"
 #endif
 int main(int argc, char **argv) {
 	int t1, t2, quit, frames, rc;
-	double delta, worldTime, fpsTimer, fps, distance, etiSpeed;
+	double delta, worldTime, fpsTimer, fps, distance, etiSpeed, posX, posY, stepX, stepY, speed_fix;
 	SDL_Event event;
 	SDL_Surface *screen, *charset;
 	SDL_Surface *eti;
@@ -102,10 +111,10 @@ int main(int argc, char **argv) {
 	SDL_Window *window;
 	SDL_Renderer *renderer;
 
-	// okno konsoli nie jest widoczne, je¿eli chcemy zobaczyæ
+	// okno konsoli nie jest widoczne, jeï¿½eli chcemy zobaczyï¿½
 	// komunikaty wypisywane printf-em trzeba w opcjach:
 	// project -> szablon2 properties -> Linker -> System -> Subsystem
-	// zmieniæ na "Console"
+	// zmieniï¿½ na "Console"
 	// console window is not visible, to see the printf output
 	// the option:
 	// project -> szablon2 properties -> Linker -> System -> Subsystem
@@ -118,7 +127,7 @@ int main(int argc, char **argv) {
 		return 1;
 		}
 
-	// tryb pe³noekranowy / fullscreen mode
+	// tryb peï¿½noekranowy / fullscreen mode
 //	rc = SDL_CreateWindowAndRenderer(0, 0, SDL_WINDOW_FULLSCREEN_DESKTOP,
 //	                                 &window, &renderer);
 	rc = SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0,
@@ -144,7 +153,7 @@ int main(int argc, char **argv) {
 	                           SCREEN_WIDTH, SCREEN_HEIGHT);
 
 
-	// wy³¹czenie widocznoœci kursora myszy
+	// wyï¿½ï¿½czenie widocznoï¿½ci kursora myszy
 	SDL_ShowCursor(SDL_DISABLE);
 
 	// wczytanie obrazka cs8x8.bmp
@@ -160,7 +169,14 @@ int main(int argc, char **argv) {
 		};
 	SDL_SetColorKey(charset, true, 0x000000);
 
-	eti = SDL_LoadBMP("./eti.bmp");
+// Define the square size
+int squareSize = 20; // Adjust the size as needed
+
+// Create an SDL_Surface for the square
+eti = SDL_CreateRGBSurface(0, squareSize, squareSize, 32, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000);
+
+// Fill the surface with a solid color (e.g., green)
+SDL_FillRect(eti, NULL, SDL_MapRGB(eti->format, 0, 255, 0)); // Green square
 	if(eti == NULL) {
 		printf("SDL_LoadBMP(eti.bmp) error: %s\n", SDL_GetError());
 		SDL_FreeSurface(charset);
@@ -177,6 +193,7 @@ int main(int argc, char **argv) {
 	int zielony = SDL_MapRGB(screen->format, 0x00, 0xFF, 0x00);
 	int czerwony = SDL_MapRGB(screen->format, 0xFF, 0x00, 0x00);
 	int niebieski = SDL_MapRGB(screen->format, 0x11, 0x11, 0xCC);
+	int direction = 1;
 
 	t1 = SDL_GetTicks();
 
@@ -184,15 +201,19 @@ int main(int argc, char **argv) {
 	fpsTimer = 0;
 	fps = 0;
 	quit = 0;
-	worldTime = 0;
-	distance = 0;
-	etiSpeed = 1;
+	// worldTime = 0;
+	// distance = 0;
+	// etiSpeed = 1;
+	// posX = SCREEN_WIDTH / 2;
+	// posY = SCREEN_HEIGHT / 2;
+	speed_fix = 10;
+	ResetGame(posX, posY, worldTime, distance, etiSpeed, direction);
 
 	while(!quit) {
 		t2 = SDL_GetTicks();
 
 		// w tym momencie t2-t1 to czas w milisekundach,
-		// jaki uplyna³ od ostatniego narysowania ekranu
+		// jaki uplynaï¿½ od ostatniego narysowania ekranu
 		// delta to ten sam czas w sekundach
 		// here t2-t1 is the time in milliseconds since
 		// the last screen was drawn
@@ -202,14 +223,30 @@ int main(int argc, char **argv) {
 
 		worldTime += delta;
 
-		distance += etiSpeed * delta;
+		stepX = etiSpeed * delta * SCREEN_WIDTH / speed_fix;
+		stepY = etiSpeed * delta * SCREEN_HEIGHT / speed_fix;
+
+		if(direction == 0){
+			posY -= stepY;
+		}
+		else if(direction == 1){
+			posX += stepX;
+		}
+		else if(direction == 2){
+			posY += stepY;
+		}
+		else {
+			posX -= stepX;
+		}
+		// distance += 0.9 * delta;
 
 		SDL_FillRect(screen, NULL, czarny);
 
 		DrawSurface(screen, eti,
-		            SCREEN_WIDTH / 2 + sin(distance) * SCREEN_HEIGHT / 3,
-			    SCREEN_HEIGHT / 2 + cos(distance) * SCREEN_HEIGHT / 3);
-
+				// SCREEN_WIDTH / 2 + (distance/50) * SCREEN_WIDTH / 3,
+				// SCREEN_WIDTH / 2 + (direction%2)*(distance/10) * SCREEN_WIDTH,
+			    // SCREEN_HEIGHT / 2 + (direction%2-1)*(distance/10) * SCREEN_HEIGHT);
+				posX, posY);
 		fpsTimer += delta;
 		if(fpsTimer > 0.5) {
 			fps = frames * 2;
@@ -223,7 +260,7 @@ int main(int argc, char **argv) {
 		sprintf(text, "Szablon drugiego zadania, czas trwania = %.1lf s  %.0lf klatek / s", worldTime, fps);
 		DrawString(screen, screen->w / 2 - strlen(text) * 8 / 2, 10, text, charset);
 		//	      "Esc - exit, \030 - faster, \031 - slower"
-		sprintf(text, "Esc - wyjscie, \030 - przyspieszenie, \031 - zwolnienie");
+		sprintf(text, "Esc - wyjscie");
 		DrawString(screen, screen->w / 2 - strlen(text) * 8 / 2, 26, text, charset);
 
 		SDL_UpdateTexture(scrtex, NULL, screen->pixels, screen->pitch);
@@ -231,24 +268,27 @@ int main(int argc, char **argv) {
 		SDL_RenderCopy(renderer, scrtex, NULL, NULL);
 		SDL_RenderPresent(renderer);
 
-		// obs³uga zdarzeñ (o ile jakieœ zasz³y) / handling of events (if there were any)
+		// obsï¿½uga zdarzeï¿½ (o ile jakieï¿½ zaszï¿½y) / handling of events (if there were any)
 		while(SDL_PollEvent(&event)) {
 			switch(event.type) {
 				case SDL_KEYDOWN:
 					if(event.key.keysym.sym == SDLK_ESCAPE) quit = 1;
-					else if(event.key.keysym.sym == SDLK_UP) etiSpeed = 2.0;
-					else if(event.key.keysym.sym == SDLK_DOWN) etiSpeed = 0.3;
+					else if(event.key.keysym.sym == SDLK_n) ResetGame(posX, posY, worldTime, distance, etiSpeed, direction);
+					else if(event.key.keysym.sym == SDLK_UP) direction = 0;
+					else if(event.key.keysym.sym == SDLK_RIGHT) direction = 1;
+					else if(event.key.keysym.sym == SDLK_DOWN) direction = 2;
+					else if(event.key.keysym.sym == SDLK_LEFT) direction = 3;
 					break;
-				case SDL_KEYUP:
-					etiSpeed = 1.0;
-					break;
+				// case SDL_KEYUP:
+				// 	etiSpeed = 1.0;
+				// 	break;
 				case SDL_QUIT:
 					quit = 1;
 					break;
 				};
 			};
 		frames++;
-		};
+	};
 
 	// zwolnienie powierzchni / freeing all surfaces
 	SDL_FreeSurface(charset);
