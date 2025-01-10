3cb6e960100f406dce5b653a31c7a0c1a0d3e2d8
Emwook <lukasiuk.emilia@gmail.com>
2025-01-10 10:45:00 +0100

    game boundary and bounds check, snake rerouting and additional game stats

--- main.cpp
+++ main.cpp
@@ -11,13 +11,7 @@ extern "C" {
 #define SCREEN_WIDTH	640
 #define SCREEN_HEIGHT	480
 
-
-// narysowanie napisu txt na powierzchni screen, zaczynaj�c od punktu (x, y)
-// charset to bitmapa 128x128 zawieraj�ca znaki
-// draw a text txt on surface screen, starting from the point (x, y)
-// charset is a 128x128 bitmap containing character images
-void DrawString(SDL_Surface *screen, int x, int y, const char *text,
-                SDL_Surface *charset) {
+void DrawString(SDL_Surface *screen, int x, int y, const char *text, SDL_Surface *charset) {
 	int px, py, c;
 	SDL_Rect s, d;
 	s.w = 8;
@@ -35,14 +29,9 @@ void DrawString(SDL_Surface *screen, int x, int y, const char *text,
 		SDL_BlitSurface(charset, &s, screen, &d);
 		x += 8;
 		text++;
-		};
 	};
+};
 
-
-// narysowanie na ekranie screen powierzchni sprite w punkcie (x, y)
-// (x, y) to punkt �rodka obrazka sprite na ekranie
-// draw a surface sprite on a surface screen in point (x, y)
-// (x, y) is the center of sprite on screen
 void DrawSurface(SDL_Surface *screen, SDL_Surface *sprite, int x, int y) {
 	SDL_Rect dest;
 	dest.x = x - sprite->w / 2;
@@ -50,21 +39,14 @@ void DrawSurface(SDL_Surface *screen, SDL_Surface *sprite, int x, int y) {
 	dest.w = sprite->w;
 	dest.h = sprite->h;
 	SDL_BlitSurface(sprite, NULL, screen, &dest);
-	};
+};
 
-
-// rysowanie pojedynczego pixela
-// draw a single pixel
 void DrawPixel(SDL_Surface *surface, int x, int y, Uint32 color) {
 	int bpp = surface->format->BytesPerPixel;
 	Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
 	*(Uint32 *)p = color;
 	};
 
-
-// rysowanie linii o d�ugo�ci l w pionie (gdy dx = 0, dy = 1) 
-// b�d� poziomie (gdy dx = 1, dy = 0)
-// draw a vertical (when dx = 0, dy = 1) or horizontal (when dx = 1, dy = 0) line
 void DrawLine(SDL_Surface *screen, int x, int y, int l, int dx, int dy, Uint32 color) {
 	for(int i = 0; i < l; i++) {
 		DrawPixel(screen, x, y, color);
@@ -73,9 +55,6 @@ void DrawLine(SDL_Surface *screen, int x, int y, int l, int dx, int dy, Uint32 c
 		};
 	};
 
-
-// rysowanie prostok�ta o d�ugo�ci bok�w l i k
-// draw a rectangle of size l by k
 void DrawRectangle(SDL_Surface *screen, int x, int y, int l, int k,
                    Uint32 outlineColor, Uint32 fillColor) {
 	int i;
@@ -87,17 +66,48 @@ void DrawRectangle(SDL_Surface *screen, int x, int y, int l, int k,
 		DrawLine(screen, x + 1, i, l - 2, 1, 0, fillColor);
 	};
 
-// own function
 void ResetGame(double &posX, double &posY, double &worldTime, double &distance, double &etiSpeed, int &direction) {
     posX = SCREEN_WIDTH / 2;
-	posY = SCREEN_HEIGHT / 2;
-	worldTime = 0;
-	distance = 0;
-	etiSpeed = 1;
-	direction = 0;
+    posY = SCREEN_HEIGHT / 2;
+    worldTime = 0;
+    distance = 0;
+    etiSpeed = 1;
+    direction = 1;
 }
 
-// main
+bool isValidMove(double nextX, double nextY, int BOARD_MARGIN, int BOARD_WIDTH, int BOARD_HEIGHT, int squareSize) {
+    double halfSquare = squareSize / 2;
+    
+    return (nextX - halfSquare >= BOARD_MARGIN &&
+            nextX + halfSquare <= BOARD_MARGIN + BOARD_WIDTH &&
+            nextY - halfSquare >= BOARD_MARGIN &&
+            nextY + halfSquare <= BOARD_MARGIN + BOARD_HEIGHT);
+}
+
+int autoTurn(int currentDirection, double posX, double posY, int BOARD_MARGIN, int BOARD_WIDTH, int BOARD_HEIGHT, int squareSize) {
+    int newDirection;
+    double testX, testY;
+    double step = squareSize;
+
+    int rightTurn = (currentDirection + 1) % 4;
+    int leftTurn = (currentDirection + 3) % 4;
+
+    testX = posX + (rightTurn == 1 ? step : (rightTurn == 3 ? -step : 0));
+    testY = posY + (rightTurn == 0 ? -step : (rightTurn == 2 ? step : 0));
+    
+    if (isValidMove(testX, testY, BOARD_MARGIN, BOARD_WIDTH, BOARD_HEIGHT, squareSize)) {
+        return rightTurn;
+    }
+
+    testX = posX + (leftTurn == 1 ? step : (leftTurn == 3 ? -step : 0));
+    testY = posY + (leftTurn == 0 ? -step : (leftTurn == 2 ? step : 0));
+    
+    if (isValidMove(testX, testY, BOARD_MARGIN, BOARD_WIDTH, BOARD_HEIGHT, squareSize)) {
+        return leftTurn;
+    }
+	return currentDirection;
+}
+
 #ifdef __cplusplus
 extern "C"
 #endif
@@ -111,14 +121,6 @@ int main(int argc, char **argv) {
 	SDL_Window *window;
 	SDL_Renderer *renderer;
 
-	// okno konsoli nie jest widoczne, je�eli chcemy zobaczy�
-	// komunikaty wypisywane printf-em trzeba w opcjach:
-	// project -> szablon2 properties -> Linker -> System -> Subsystem
-	// zmieni� na "Console"
-	// console window is not visible, to see the printf output
-	// the option:
-	// project -> szablon2 properties -> Linker -> System -> Subsystem
-	// must be changed to "Console"
 	printf("wyjscie printfa trafia do tego okienka\n");
 	printf("printf output goes here\n");
 
@@ -127,9 +129,6 @@ int main(int argc, char **argv) {
 		return 1;
 		}
 
-	// tryb pe�noekranowy / fullscreen mode
-//	rc = SDL_CreateWindowAndRenderer(0, 0, SDL_WINDOW_FULLSCREEN_DESKTOP,
-//	                                 &window, &renderer);
 	rc = SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0,
 	                                 &window, &renderer);
 	if(rc != 0) {
@@ -153,10 +152,8 @@ int main(int argc, char **argv) {
 	                           SCREEN_WIDTH, SCREEN_HEIGHT);
 
 
-	// wy��czenie widoczno�ci kursora myszy
 	SDL_ShowCursor(SDL_DISABLE);
 
-	// wczytanie obrazka cs8x8.bmp
 	charset = SDL_LoadBMP("./cs8x8.bmp");
 	if(charset == NULL) {
 		printf("SDL_LoadBMP(cs8x8.bmp) error: %s\n", SDL_GetError());
@@ -169,14 +166,11 @@ int main(int argc, char **argv) {
 		};
 	SDL_SetColorKey(charset, true, 0x000000);
 
-// Define the square size
-int squareSize = 20; // Adjust the size as needed
+	int squareSize = 20;
 
-// Create an SDL_Surface for the square
-eti = SDL_CreateRGBSurface(0, squareSize, squareSize, 32, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000);
+	eti = SDL_CreateRGBSurface(0, squareSize, squareSize, 32, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000);
 
-// Fill the surface with a solid color (e.g., green)
-SDL_FillRect(eti, NULL, SDL_MapRGB(eti->format, 0, 255, 0)); // Green square
+	SDL_FillRect(eti, NULL, SDL_MapRGB(eti->format, 0, 255, 0)); 
 	if(eti == NULL) {
 		printf("SDL_LoadBMP(eti.bmp) error: %s\n", SDL_GetError());
 		SDL_FreeSurface(charset);
@@ -193,6 +187,16 @@ SDL_FillRect(eti, NULL, SDL_MapRGB(eti->format, 0, 255, 0)); // Green square
 	int zielony = SDL_MapRGB(screen->format, 0x00, 0xFF, 0x00);
 	int czerwony = SDL_MapRGB(screen->format, 0xFF, 0x00, 0x00);
 	int niebieski = SDL_MapRGB(screen->format, 0x11, 0x11, 0xCC);
+
+
+	const int BOARD_MARGIN = 80;
+    const int BOARD_WIDTH = SCREEN_WIDTH - (2 * BOARD_MARGIN);
+    const int BOARD_HEIGHT = SCREEN_HEIGHT - (2 * BOARD_MARGIN);
+    const int STATS_HEIGHT = 60;
+    const int STATS_Y = SCREEN_HEIGHT - STATS_HEIGHT - 4;
+    const int STATS_X = 4;
+    const int STATS_WIDTH = SCREEN_WIDTH - 8;
+
 	int direction = 1;
 
 	t1 = SDL_GetTicks();
@@ -201,23 +205,11 @@ SDL_FillRect(eti, NULL, SDL_MapRGB(eti->format, 0, 255, 0)); // Green square
 	fpsTimer = 0;
 	fps = 0;
 	quit = 0;
-	// worldTime = 0;
-	// distance = 0;
-	// etiSpeed = 1;
-	// posX = SCREEN_WIDTH / 2;
-	// posY = SCREEN_HEIGHT / 2;
 	speed_fix = 10;
 	ResetGame(posX, posY, worldTime, distance, etiSpeed, direction);
 
 	while(!quit) {
 		t2 = SDL_GetTicks();
-
-		// w tym momencie t2-t1 to czas w milisekundach,
-		// jaki uplyna� od ostatniego narysowania ekranu
-		// delta to ten sam czas w sekundach
-		// here t2-t1 is the time in milliseconds since
-		// the last screen was drawn
-		// delta is the same time in seconds
 		delta = (t2 - t1) * 0.001;
 		t1 = t2;
 
@@ -226,27 +218,42 @@ SDL_FillRect(eti, NULL, SDL_MapRGB(eti->format, 0, 255, 0)); // Green square
 		stepX = etiSpeed * delta * SCREEN_WIDTH / speed_fix;
 		stepY = etiSpeed * delta * SCREEN_HEIGHT / speed_fix;
 
-		if(direction == 0){
-			posY -= stepY;
-		}
-		else if(direction == 1){
-			posX += stepX;
-		}
-		else if(direction == 2){
-			posY += stepY;
-		}
-		else {
-			posX -= stepX;
-		}
-		// distance += 0.9 * delta;
+		double nextX = posX;
+        double nextY = posY;
+        
+        if(direction == 0) nextY -= stepY;
+        else if(direction == 1) nextX += stepX;
+        else if(direction == 2) nextY += stepY;
+        else nextX -= stepX;
+
+		if (!isValidMove(nextX, nextY, BOARD_MARGIN, BOARD_WIDTH, BOARD_HEIGHT, squareSize)) {
+            direction = autoTurn(direction, posX, posY, BOARD_MARGIN, BOARD_WIDTH, BOARD_HEIGHT, squareSize);
+            
+            nextX = posX;
+            nextY = posY;
+            if(direction == 0) nextY -= stepY;
+            else if(direction == 1) nextX += stepX;
+            else if(direction == 2) nextY += stepY;
+            else nextX -= stepX;
+        }
+		posX = nextX;
+        posY = nextY;
+
 
 		SDL_FillRect(screen, NULL, czarny);
+		DrawRectangle(screen, BOARD_MARGIN, BOARD_MARGIN, BOARD_WIDTH, BOARD_HEIGHT, zielony, czarny); 
 
-		DrawSurface(screen, eti,
-				// SCREEN_WIDTH / 2 + (distance/50) * SCREEN_WIDTH / 3,
-				// SCREEN_WIDTH / 2 + (direction%2)*(distance/10) * SCREEN_WIDTH,
-			    // SCREEN_HEIGHT / 2 + (direction%2-1)*(distance/10) * SCREEN_HEIGHT);
-				posX, posY);
+		DrawRectangle(screen, STATS_X, STATS_Y, STATS_WIDTH, STATS_HEIGHT, czerwony, niebieski);
+
+		sprintf(text, "Score: %d", 0);
+		DrawString(screen, STATS_X + (STATS_WIDTH/2) - BOARD_MARGIN, STATS_Y + BOARD_MARGIN/2, text, charset);
+
+		sprintf(text, "Length: %d", 1);
+		DrawString(screen, STATS_X + STATS_WIDTH - BOARD_MARGIN, STATS_Y + BOARD_MARGIN/2, text, charset);
+
+		sprintf(text, "ESC - quit, N - new game");
+		DrawString(screen, STATS_X, STATS_Y + BOARD_MARGIN/2, text, charset);
+		DrawSurface(screen, eti, posX, posY);
 		fpsTimer += delta;
 		if(fpsTimer > 0.5) {
 			fps = frames * 2;
@@ -254,21 +261,15 @@ SDL_FillRect(eti, NULL, SDL_MapRGB(eti->format, 0, 255, 0)); // Green square
 			fpsTimer -= 0.5;
 			};
 
-		// tekst informacyjny / info text
 		DrawRectangle(screen, 4, 4, SCREEN_WIDTH - 8, 36, czerwony, niebieski);
-		//            "template for the second project, elapsed time = %.1lf s  %.0lf frames / s"
-		sprintf(text, "Szablon drugiego zadania, czas trwania = %.1lf s  %.0lf klatek / s", worldTime, fps);
+		sprintf(text, "czas trwania = %.1lf s  %.0lf klatek / s", worldTime, fps);
 		DrawString(screen, screen->w / 2 - strlen(text) * 8 / 2, 10, text, charset);
-		//	      "Esc - exit, \030 - faster, \031 - slower"
-		sprintf(text, "Esc - wyjscie");
-		DrawString(screen, screen->w / 2 - strlen(text) * 8 / 2, 26, text, charset);
 
 		SDL_UpdateTexture(scrtex, NULL, screen->pixels, screen->pitch);
 //		SDL_RenderClear(renderer);
 		SDL_RenderCopy(renderer, scrtex, NULL, NULL);
 		SDL_RenderPresent(renderer);
 
-		// obs�uga zdarze� (o ile jakie� zasz�y) / handling of events (if there were any)
 		while(SDL_PollEvent(&event)) {
 			switch(event.type) {
 				case SDL_KEYDOWN:
@@ -279,9 +280,6 @@ SDL_FillRect(eti, NULL, SDL_MapRGB(eti->format, 0, 255, 0)); // Green square
 					else if(event.key.keysym.sym == SDLK_DOWN) direction = 2;
 					else if(event.key.keysym.sym == SDLK_LEFT) direction = 3;
 					break;
-				// case SDL_KEYUP:
-				// 	etiSpeed = 1.0;
-				// 	break;
 				case SDL_QUIT:
 					quit = 1;
 					break;
@@ -290,7 +288,6 @@ SDL_FillRect(eti, NULL, SDL_MapRGB(eti->format, 0, 255, 0)); // Green square
 		frames++;
 	};
 
-	// zwolnienie powierzchni / freeing all surfaces
 	SDL_FreeSurface(charset);
 	SDL_FreeSurface(screen);
 	SDL_DestroyTexture(scrtex);
