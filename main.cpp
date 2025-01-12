307e1dc4d0c7ca3ee81cd0fb1b6ca0e66ebe73bb
Emwook <lukasiuk.emilia@gmail.com>
2025-01-12 20:30:03 +0100

    most of red dot mechanics done, a few more details need attention to count it

--- main.cpp
+++ main.cpp
@@ -25,11 +25,20 @@ extern "C" {
 #define MAX_SNAKE_LENGTH 20
 #define MIN_SNAKE_LENGTH 3
 
+#define DEFAULT_SNAKE_SPEED 1
+#define DEFAULT_SNAKE_LENGTH 3
+#define MIN_SPAWN_INTERVAL 5.0f
+#define MAX_SPAWN_INTERVAL 10.0f
+#define DISPLAY_DURATION   10.0f
+#define SHORTEN_LENGTH     1
+#define SLOWDOWN_FACTOR    0.3
+#define PROGRESS_BAR_WIDTH 20
+#define PROGRESS_BAR_HEIGHT 100
+
 typedef enum {
     GAME_RUNNING,
     GAME_OVER
 } GameState;
-
 
 typedef struct {
     int x;
@@ -42,6 +51,14 @@ typedef struct {
     int direction;
     double speed;
 } snake_t;
+
+typedef struct {
+    SDL_Point position;
+    float spawnTime;
+    float displayDuration;
+    int isActive;
+    SDL_Surface* red_dot;
+} red_dot_t;
 
 
 void DrawString(SDL_Surface *screen, int x, int y, const char *text, SDL_Surface *charset) {
@@ -98,7 +115,7 @@ void DrawRectangle(SDL_Surface *screen, int x, int y, int l, int k, Uint32 outli
 		DrawLine(screen, x + 1, i, l - 2, 1, 0, fillColor);
 	};
 
-void ResetGame(double &worldTime, snake_t &snake) {
+void ResetGame(double &worldTime, snake_t &snake) { // add red and blue dot reset
 	snake.length = MIN_SNAKE_LENGTH;
 	for(int i = 0; i<snake.length; i++){
 		snake.body[i].x = SCREEN_WIDTH / 2 - (i *SQUARE_SIZE);
@@ -109,9 +126,10 @@ void ResetGame(double &worldTime, snake_t &snake) {
     worldTime = 0;
 }
 
-int isValidMove(double nextX, double nextY) {
-    double halfSquare = SQUARE_SIZE / 2;
-    
+int isValidMove(position_t nextPos) {
+    int halfSquare = SQUARE_SIZE / 2;
+    int nextX = nextPos.x;
+    int nextY = nextPos.y;
     return (nextX - halfSquare >= BOARD_MARGIN &&
             nextX + halfSquare <= BOARD_MARGIN + BOARD_WIDTH &&
             nextY - halfSquare >= BOARD_MARGIN &&
@@ -119,52 +137,54 @@ int isValidMove(double nextX, double nextY) {
 }
 
 void autoTurn(snake_t &snake) {
-    double step = SQUARE_SIZE;
+    int step = SQUARE_SIZE;
 	int rightTurn = (snake.direction + 1) % 4;
     int leftTurn = (snake.direction + 3) % 4;
     
-    double rightX = snake.body[0].x;
-    double rightY = snake.body[0].y;
-    
+    position_t rightPos = {
+        .x =  snake.body[0].x,
+        .y = snake.body[0].y,
+    };
     switch(rightTurn) {
         case 0:
-            rightY -= step;
+            rightPos.y -= step;
             break;
         case 1:
-            rightX += step;
+            rightPos.x += step;
             break;
         case 2:
-            rightY += step;
+            rightPos.y += step;
             break;
         case 3:
-            rightX -= step;
+            rightPos.x -= step;
             break;
     }
     
-    double leftX = snake.body[0].x;
-    double leftY = snake.body[0].y;
-    
+    position_t leftPos = {
+        .x =  snake.body[0].x,
+        .y = snake.body[0].y,
+    };
     switch(leftTurn) {
         case 0:
-            leftY -= step;
+            leftPos.y -= step;
             break;
         case 1:
-            leftX += step;
+            leftPos.x += step;
             break;
         case 2:
-            leftY += step;
+            leftPos.y += step;
             break;
         case 3:
-            leftX -= step;
+            leftPos.x -= step;
             break;
     }
     
-    if (isValidMove(rightX, rightY)) {
+    if (isValidMove(rightPos)) {
         snake.direction = rightTurn;
         return;
     }
     
-    if (isValidMove(leftX, leftY)) {
+    if (isValidMove(leftPos)) {
         snake.direction = leftTurn;
         return;
     }
@@ -199,7 +219,7 @@ void UpdateSnake(snake_t &snake) {
     }
 }
 
-int checkSnakeCollision(snake_t* snake, int nextX, int nextY) {
+int checkSnakeCollision(snake_t* snake, int nextX, int nextY) { //revise for short boy
     for (int i = 1; i < snake->length - 1; i++) {
         if (nextX == snake->body[i].x && nextY == snake->body[i].y) {
             return 1;
@@ -207,16 +227,30 @@ int checkSnakeCollision(snake_t* snake, int nextX, int nextY) {
     }
 	return 0;
 }
-void randomizePosition(position_t &pos) {
-    pos.x = BOARD_MARGIN + (rand() % (BOARD_WIDTH / SQUARE_SIZE)) * SQUARE_SIZE + SQUARE_SIZE / 2;
-    pos.y = BOARD_MARGIN + (rand() % (BOARD_HEIGHT / SQUARE_SIZE)) * SQUARE_SIZE + SQUARE_SIZE / 2;
+
+void randomizePosition(position_t &pos, snake_t *snake) {
+    int validPosition;
+    int i;
+
+    do {
+        pos.x = BOARD_MARGIN + (rand() % (BOARD_WIDTH / SQUARE_SIZE)) * SQUARE_SIZE + SQUARE_SIZE / 2;
+        pos.y = BOARD_MARGIN + (rand() % (BOARD_HEIGHT / SQUARE_SIZE)) * SQUARE_SIZE + SQUARE_SIZE / 2;
+
+        validPosition = 1;
+        for (i = 0; i < snake->length; i++) {
+            if (pos.x == snake->body[i].x && pos.y == snake->body[i].y) {
+                validPosition = 0;
+                break;
+            }
+        }
+    } while (!validPosition);
 }
 
 
 void checkBlueDot(snake_t* snake, int nextX, int nextY, position_t &blue_dot_pos) {
-	if (nextX == blue_dot_pos.x && nextY == blue_dot_pos.y) {
+	if (nextX == blue_dot_pos.x && nextY == blue_dot_pos.y &&snake->length <=MAX_SNAKE_LENGTH) {
 		snake->length++;
-		randomizePosition(blue_dot_pos);
+		randomizePosition(blue_dot_pos, snake);
 	}
 }
 
@@ -291,6 +325,104 @@ void drawCircle(SDL_Surface* surface, int centerX, int centerY, int radius, Uint
     }
 }
 
+void spawnRedDot(red_dot_t* redDot, snake_t* snake, float worldTime) { // add a check for no blue and red dot collision!
+    if (redDot->isActive) return;
+
+    SDL_Point newPos;
+    int validPosition = 0;
+    while (!validPosition) {
+        newPos.x = BOARD_MARGIN + (rand() % (BOARD_WIDTH / SQUARE_SIZE)) * SQUARE_SIZE + SQUARE_SIZE / 2;
+        newPos.y = BOARD_MARGIN + (rand() % (BOARD_WIDTH / SQUARE_SIZE)) * SQUARE_SIZE + SQUARE_SIZE / 2;
+        validPosition = 1;
+        for (int i = 0; i < snake->length; i++) {
+            if (newPos.x == snake->body[i].x && newPos.y == snake->body[i].y) {
+                validPosition = 1;
+                break;
+            }
+        }
+    }
+
+    redDot->position = newPos;
+    redDot->spawnTime = worldTime;
+    redDot->isActive = 1;
+}
+
+void checkRedDot(snake_t* snake, red_dot_t* redDot) {
+    if (!redDot->isActive) return;
+
+    if (redDot->position.x == snake->body[0].x && 
+        snake->body[0].y == redDot->position.y) {
+        if (rand() % 2 == 0) {
+            int newLength = snake->length - SHORTEN_LENGTH;
+            if (newLength < 1) newLength = 1;
+            snake->length = newLength;
+        } else {
+            if(snake->speed - SLOWDOWN_FACTOR>1){
+                snake->speed -= SLOWDOWN_FACTOR;
+            }
+            else {
+                snake->speed = DEFAULT_SNAKE_SPEED;
+            }
+        }
+        redDot->isActive = 0;
+    }
+}
+
+void drawProgressBar(SDL_Surface* screen, red_dot_t* redDot, float worldTime, Uint32 color, Uint32 czarny, SDL_Surface* charset) { // bad progress bar height
+    if (!redDot->isActive) return;
+
+    float timeElapsed = worldTime - redDot->spawnTime;
+    float remainingTime = redDot->displayDuration - timeElapsed;
+
+    if (remainingTime <= 0) {
+        redDot->isActive = 0;
+        return;
+    }
+
+    SDL_Rect borderRect = {
+        PROGRESS_BAR_WIDTH *2,
+        PROGRESS_BAR_HEIGHT *2,
+        PROGRESS_BAR_WIDTH,
+        PROGRESS_BAR_HEIGHT
+    };
+    DrawRectangle(screen, borderRect.x, borderRect.y, 
+                  borderRect.w, borderRect.h, color, czarny);
+
+    float fillHeight = (remainingTime / redDot->displayDuration) * (PROGRESS_BAR_HEIGHT/3);
+    SDL_Rect fillRect = {
+        borderRect.x + 2,
+        borderRect.y + 2 + (int)((PROGRESS_BAR_HEIGHT - 4) - fillHeight),
+        borderRect.w - 4,
+        (int)fillHeight
+    };
+    SDL_FillRect(screen, &fillRect, color);
+
+    char timeText[32];
+    snprintf(timeText, sizeof(timeText), "%.1fs", remainingTime);
+
+    DrawString(screen, borderRect.x, borderRect.y + PROGRESS_BAR_HEIGHT + 10, timeText, charset);
+}
+
+
+void updateBonusSystem(snake_t* snake, red_dot_t* redDot, float worldTime) {
+    if (!redDot->isActive && worldTime >= redDot->spawnTime) {
+        spawnRedDot(redDot, snake, worldTime);
+        float spawnInterval = MIN_SPAWN_INTERVAL + 
+            (rand()%2) * 
+            (MAX_SPAWN_INTERVAL- MIN_SPAWN_INTERVAL);
+        redDot->spawnTime = worldTime + spawnInterval;
+    }
+
+    checkRedDot(snake, redDot);
+
+    if (redDot->isActive && 
+        (worldTime - redDot->spawnTime) >= redDot->displayDuration) {
+        redDot->isActive = 0;
+    }
+}
+
+
+
 
 
 #ifdef __cplusplus
@@ -308,18 +440,24 @@ int main(int argc, char **argv) {
 
     snake_t snake;
     snake.direction = 1;
-    snake.length = 3;
-    snake.speed = 1;
+    snake.length = DEFAULT_SNAKE_LENGTH;
+    snake.speed = DEFAULT_SNAKE_SPEED;
     ResetGame(worldTime, snake);
 
 	position_t blue_dot_pos;
-	randomizePosition(blue_dot_pos);
+	randomizePosition(blue_dot_pos, &snake);
+
+    red_dot_t redDot = {
+        .isActive = 0,
+        .displayDuration = 5,
+    };
 
     SDL_Event event;
     SDL_Surface *screen, *charset;
     SDL_Surface *eti;
 	SDL_Surface *ite;
 	SDL_Surface *blue_dot;
+    SDL_Surface *red_dot;
     SDL_Texture *scrtex;
     SDL_Window *window;
     SDL_Renderer *renderer;
@@ -370,10 +508,13 @@ int main(int argc, char **argv) {
     eti = SDL_CreateRGBSurface(0, SQUARE_SIZE, SQUARE_SIZE, 32, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000);
 	ite = SDL_CreateRGBSurface(0, SQUARE_SIZE, SQUARE_SIZE, 32, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000);
 	blue_dot = SDL_CreateRGBSurface(0, SQUARE_SIZE, SQUARE_SIZE, 32, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000);
+    red_dot = SDL_CreateRGBSurface(0, SQUARE_SIZE, SQUARE_SIZE, 32, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000);
     SDL_FillRect(eti, NULL, SDL_MapRGB(eti->format, 255, 255, 0)); 
 	SDL_FillRect(ite, NULL, SDL_MapRGB(ite->format, 0, 255, 0)); 
 	drawCircle(blue_dot, SQUARE_SIZE / 2, SQUARE_SIZE / 2, SQUARE_SIZE / 3, SDL_MapRGB(blue_dot->format, 0, 0, 255));
-	SDL_Texture* tex = SDL_CreateTextureFromSurface(renderer, blue_dot);
+    drawCircle(red_dot, SQUARE_SIZE / 2, SQUARE_SIZE / 2, SQUARE_SIZE / 3, SDL_MapRGB(red_dot->format, 255, 0, 0));
+	SDL_Texture* blueTex = SDL_CreateTextureFromSurface(renderer, blue_dot);
+    SDL_Texture* redTex = SDL_CreateTextureFromSurface(renderer, red_dot);
 
     if(eti == NULL) {
         printf("SDL_LoadBMP(eti.bmp) error: %s\n", SDL_GetError());
@@ -433,14 +574,18 @@ int main(int argc, char **argv) {
             }
 			int nextX = snake.body[0].x + stepX;
 			int nextY =  snake.body[0].y + stepY;
-			if (!isValidMove(nextX, nextY)) {
+            position_t nextPos = {
+                .x = nextX,
+                .y = nextY,
+            };
+			if (!isValidMove(nextPos)) {
                 autoTurn(snake);
             }
 			if(checkSnakeCollision(&snake, nextX, nextY)){
 				gameState = GAME_OVER;
 			}
 			checkBlueDot(&snake, nextX, nextY, blue_dot_pos);
-
+            updateBonusSystem(&snake,&redDot,worldTime);
 			UpdateSnake(snake);
             timeAccumulator -= (SQUARE_SIZE / (snake.speed * SPEED_FIX));
         }
@@ -462,7 +607,11 @@ int main(int argc, char **argv) {
 		for (int i = 1; i <snake.length; i++) {
         	DrawSurface(screen, ite, snake.body[i].x, snake.body[i].y);
 		}
-		DrawSurface(screen, blue_dot, blue_dot_pos.x, blue_dot_pos.y);
+		DrawSurface(screen, blue_dot, blue_dot_pos.x, blue_dot_pos.y);\
+        if(redDot.isActive == 1){
+            DrawSurface(screen, red_dot, redDot.position.x, redDot.position.y);
+            drawProgressBar(screen,&redDot,worldTime,zielony,czarny, charset);
+        }
         fpsTimer += delta;
 
         if(fpsTimer > 0.5) {
